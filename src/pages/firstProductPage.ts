c561d7d831cccee9c11e21c3b2f21b2c9fc66e9e
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2025-05-18 18:13:37 +0200

    added test Weryfikacja widocznosci komunikatu o dodaniu produktu do koszyka in firstProductPage.spec.ts file

--- src/pages/firstProductPage.ts
+++ src/pages/firstProductPage.ts
@@ -15,4 +15,21 @@ export class FirstProductPage {
       return false;
     }
   }
+
+  async clickGoToBasketButton(): Promise<void> {
+    const goToBasketButton = this.page.locator('button[name="add-to-cart"]', { hasText: 'Dodaj do koszyka' });
+    await goToBasketButton.click();
+  }
+
+  async isAddedToBasketMessageDisplayed(): Promise<boolean> {
+    try {
+      await expect(
+      this.page.locator('div.woocommerce-message', { hasText: 'Piłka nożna KIPSTA F100' })
+      ).toBeVisible();
+      return true;
+    } catch {
+      return false;
+    }
+  }
+
 }
--- src/tests/firstProductPage.spec.ts
+++ src/tests/firstProductPage.spec.ts
@@ -12,3 +12,15 @@ test('Weryfikacja tytulu strony Pilka Kipsta 100', async ({
 
     expect(await firstProductPage.isTitleMatches()).toBe(true);
   });
+
+test('Weryfikacja widocznosci komunikatu o dodaniu produktu do koszyka', async ({ 
+  mainPage,
+  firstProductPage
+ }) => {
+  
+    await mainPage.goto();
+    await mainPage.clickFirstProductLink();
+    await firstProductPage.clickGoToBasketButton();
+
+    expect(await firstProductPage.isAddedToBasketMessageDisplayed()).toBe(true);
+  });
