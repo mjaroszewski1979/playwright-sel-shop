81c457e7c79f7a8e1ceaab4315ab4e4a1139a889
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2025-05-19 21:00:45 +0200

    refactoring of koszykPage.ts file for cleaner and well structured code

--- src/pages/koszykPage.ts
+++ src/pages/koszykPage.ts
@@ -1,11 +1,25 @@
-import { Page, expect } from '@playwright/test';
+import { Page, expect, Locator } from '@playwright/test';
 
 export class KoszykPage {
     readonly page: Page;
 
+    // Lokatory
+    readonly productLink: Locator;
+    readonly quantityInput: Locator;
+    readonly removeLink: Locator;
+    readonly emptyCartMessage: Locator;
+
     constructor(page: Page) {
         this.page = page;
+
+        // Inicjalizacja lokatorów
+        this.productLink = page.getByRole('link', { name: 'Piłka nożna KIPSTA F100', exact: true });
+        this.quantityInput = page.locator('div.quantity input[type="number"]');
+        this.removeLink = page.locator('a.remove[aria-label="Usuń produkt"]');
+        this.emptyCartMessage = page.locator('p.cart-empty.woocommerce-info');
     }
+
+    
 
     async isTitleMatches(): Promise<boolean> {
     try {
@@ -18,9 +32,7 @@ export class KoszykPage {
     
     async isFirstProductLinkDisplayed(): Promise<boolean> {
     try {
-      await expect(
-      this.page.getByRole('link', { name: 'Piłka nożna KIPSTA F100', exact: true })
-      ).toBeVisible();
+      await expect(this.productLink).toBeVisible();
       return true;
     } catch {
       return false;
@@ -29,8 +41,7 @@ export class KoszykPage {
 
     async isNumberOfProductsCorrect(): Promise<boolean> {
     try {
-      const inputElement = this.page.locator('div.quantity input[type="number"]');
-      const value = await inputElement.getAttribute('value'); 
+      const value = await this.quantityInput.getAttribute('value');
       expect(value).toBe('2');
       return true;
     } catch {
@@ -39,15 +50,13 @@ export class KoszykPage {
   }
 
   async clickRemoveProductLink(): Promise<void> {
-    const removeProductLink = this.page.locator('a.remove[aria-label="Usuń produkt"]');
-    removeProductLink.click();
+    await this.removeLink.click();
   }
 
   async isRemovedProductMessageDisplayed(): Promise<boolean> {
     try {
-      const emptyCartMessage = this.page.locator('p.cart-empty.woocommerce-info');
-      await expect(emptyCartMessage).toBeVisible();
-      await expect(emptyCartMessage).toContainText('Twój koszyk jest pusty.');
+      await expect(this.emptyCartMessage).toBeVisible();
+      await expect(this.emptyCartMessage).toContainText('Twój koszyk jest pusty.');
       return true;
     } catch {
       return false;
