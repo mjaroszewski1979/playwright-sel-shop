9f616bc2e5048b851ab12d1b0bec691c7e862db2
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2025-06-06 20:07:35 +0200

    added comments in plikiDoPobraniaPage.spec.ts file

--- src/tests/plikiDoPobraniaPage.spec.ts
+++ src/tests/plikiDoPobraniaPage.spec.ts
@@ -2,8 +2,17 @@ import { test, expect } from '../pages/basePage';
 import { config } from '../utils/config';
 
 
-
-test('Weryfikacja url strony Pliki Do Pobrania', async ({ 
+/**
+ * Test to verify that the user is correctly redirected to the "Downloads" page after logging in.
+ *
+ * Steps:
+ * 1. Navigate to the home page.
+ * 2. Go to the "My Account" page.
+ * 3. Log in with valid credentials.
+ * 4. Click the "Downloads" link in the account menu.
+ * 5. Assert that the current URL matches the expected URL for the "Downloads" page.
+ */
+test('Verify URL of Downloads page', async ({ 
   mainPage,
   mojeKontoPage,
   plikiDoPobraniaPage,
@@ -17,7 +26,17 @@ test('Weryfikacja url strony Pliki Do Pobrania', async ({
     expect(await plikiDoPobraniaPage.verifyUserIsOnPlikiDoPobraniaPage()).toBe(true);
   });
   
-test('Weryfikacja widocznosci i tekstu wyswietlonego komunikatu', async ({ 
+/**
+ * Test to verify that the expected information message is correctly displayed on the "Downloads" page.
+ *
+ * Steps:
+ * 1. Navigate to the home page.
+ * 2. Go to the "My Account" page.
+ * 3. Log in with valid credentials.
+ * 4. Click the "Downloads" link in the account menu.
+ * 5. Assert that the informational message is visible and contains the correct text.
+ */
+test('Verify visibility and correctness of the info message on Downloads page', async ({ 
   mainPage,
   mojeKontoPage,
   plikiDoPobraniaPage,
