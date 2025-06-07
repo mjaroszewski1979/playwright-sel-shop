c0fe420df8aac429fec53ab53f75f86994507500
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2025-06-07 08:53:59 +0200

    added comments in adresyPage.ts file

--- src/pages/adresyPage.ts
+++ src/pages/adresyPage.ts
@@ -4,6 +4,10 @@ import { generateRandomStreet } from '../utils/actions';
 import { isUrlMatches } from '../utils/urlUtils';
 import { clickElement } from '../utils/actions';
 
+/**
+ * Page Object Model class for the "Adresy" page.
+ * Encapsulates locators and operations for billing and shipping address management.
+ */
 export class AdresyPage {
     readonly page: Page;
 
@@ -17,10 +21,14 @@ export class AdresyPage {
     readonly saveButton: Locator;
     readonly addressFirstSection: Locator;
 
+    /**
+   * Initializes page locators.
+   * @param page - The Playwright Page instance.
+   */
     constructor(page: Page) {
         this.page = page;
 
-        // Locators initialization
+        // Initialize locators
         this.adresRozliczeniowyHeader = page.locator('header.title > h3').nth(0);
         this.adresDoWysylkiHeader = page.locator('header.title > h3').nth(1);
         this.adresRozliczeniowyEdycjaLink = page.locator('a.edit').nth(0);
@@ -31,12 +39,20 @@ export class AdresyPage {
         this.addressFirstSection = page.locator('div.u-column1 > address').nth(0);
     }
 
+    /**
+   * Checks whether the user is on the "Adresy" page by verifying the URL.
+   * @returns True if URL matches expected address page URL, otherwise false.
+   */
     async verifyUserIsOnAdresyPage(): Promise<boolean> {
       
         return await isUrlMatches(this.page, 'http://www.selenium-shop.pl/moje-konto/edit-address/');
         }
 
 
+    /**
+   * Verifies that both billing and shipping address headers are visible and contain correct text.
+   * @returns True if both headers are correct, otherwise false.
+   */
     async isEditAddressSectionDisplayedCorrectly(): Promise<boolean> {
         try {
             const adresRozliczeniowyCorrect = await isElementVisibleWithText(this.adresRozliczeniowyHeader, 'Adres rozliczeniowy');
@@ -49,6 +65,10 @@ export class AdresyPage {
         }
 
 
+    /**
+   * Edits the billing address with a randomly generated street name and verifies if the update is reflected correctly.
+   * @returns True if both success message and new address are displayed correctly, otherwise false.
+   */
     async isEditingBillingAddressWorksCorrectly(): Promise<boolean> {
         try {
             await this.adresRozliczeniowyEdycjaLink.click();
@@ -64,7 +84,5 @@ export class AdresyPage {
             return false;
         }
         }
-
-    
 
 }
