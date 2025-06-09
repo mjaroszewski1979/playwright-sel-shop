ed5ee7ec8c14b0f21f138b4e506af148fe66f95f
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2025-06-09 20:22:02 +0200

    added comments in mojeKontoPage.ts file

--- src/pages/mojeKontoPage.ts
+++ src/pages/mojeKontoPage.ts
@@ -3,6 +3,9 @@ import { config } from '../utils/config';
 import { isUrlMatches } from '../utils/urlUtils';
 import { clickElement } from '../utils/actions';
 
+/**
+ * Page Object Model for the "Moje Konto" (My Account) page.
+ */
 export class MojeKontoPage {
   readonly page: Page;
 
@@ -18,6 +21,10 @@ export class MojeKontoPage {
   readonly adresyLink: Locator;
   readonly plikiDoPobraniaLink: Locator;
 
+   /**
+   * Constructor for MojeKontoPage.
+   * @param page - Playwright Page instance.
+   */
   constructor(page: Page) {
     this.page = page;
 
@@ -35,11 +42,20 @@ export class MojeKontoPage {
 
   }
 
+  /**
+   * Checks if the user is on the "Moje Konto" (My Account) page.
+   * @returns true if the URL matches the expected account page, false otherwise
+   */
    async verifyUserIsOnMojeKontoPage(): Promise<boolean> {
         
           return await isUrlMatches(this.page, 'http://www.selenium-shop.pl/moje-konto/');
           }
 
+  /**
+   * Performs login using provided credentials.
+   * @param username - The username to log in with
+   * @param password - The password to log in with
+   */
   async login(username: string, password: string): Promise<void> {
 
     await this.usernameField.fill(''); 
@@ -51,6 +67,10 @@ export class MojeKontoPage {
     await clickElement(this.loginButton);
   }
 
+  /**
+   * Verifies successful login using correct credentials.
+   * @returns true if login success message is displayed, false otherwise
+   */
   async isLoginSuccessfull(): Promise<boolean> {
     try {
       await this.login(config.username, config.password);
@@ -61,6 +81,10 @@ export class MojeKontoPage {
     }
   }
 
+  /**
+   * Verifies that incorrect login credentials trigger the expected error message.
+   * @returns true if error message is displayed, false otherwise
+   */
   async isIncorrectLoginResolvedProperly(): Promise<boolean> {
     try {
       await this.login('wronguser', 'wrongpass');
@@ -71,6 +95,10 @@ export class MojeKontoPage {
     }
   }
 
+  /**
+   * Verifies that the user can successfully log out.
+   * @returns true if logout is successful and login button is visible again
+   */
   async isLogoutSuccessfull(): Promise<boolean> {
     try {
       await this.login(config.username, config.password);
@@ -82,18 +110,30 @@ export class MojeKontoPage {
     }
   }
 
+  /**
+   * Clicks the "Adresy" (Addresses) link in the account menu.
+   */
   async clickAdresyLink(): Promise<void> {
     await clickElement(this.adresyLink);
   }
 
+  /**
+   * Clicks the "Pliki do pobrania" (Downloads) link in the account menu.
+   */
   async clickPlikiDoPobraniaLink(): Promise<void> {
     await clickElement(this.plikiDoPobraniaLink);
   }
 
+  /**
+   * Clicks the "Szczegóły konta" (Account Details) link in the account menu.
+   */
   async clickSzczegolyKontaLink(): Promise<void> {
     await clickElement(this.szczegolyKontaLink);
   }
 
+  /**
+   * Clicks the "Zamówienia" (Orders) link in the account menu.
+   */
   async clickZamowieniaLink(): Promise<void> {
     await clickElement(this.zamowieniaLink);
   }
