feb0a5a459a25a56ef5be6eccda1347779105f5b
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2025-06-09 20:29:00 +0200

    added comments in plikiDoPobraniaPage.ts file

--- src/pages/plikiDoPobraniaPage.ts
+++ src/pages/plikiDoPobraniaPage.ts
@@ -2,6 +2,9 @@ import { Page, Locator } from '@playwright/test';
 import { isElementVisibleWithPartialText } from '../utils/assertions';
 import { isUrlMatches } from '../utils/urlUtils';
 
+/**
+ * Page Object Model for the "Downloads" (Pliki do pobrania) page in the Selenium Shop application.
+ */
 export class PlikiDoPobraniaPage {
     readonly page: Page;
 
@@ -9,6 +12,10 @@ export class PlikiDoPobraniaPage {
     readonly infoMessageDiv: Locator;
 
 
+    /**
+   * Constructor for PlikiDoPobraniaPage.
+   * @param page - Playwright Page instance.
+   */
     constructor(page: Page) {
         this.page = page;
 
@@ -17,12 +24,20 @@ export class PlikiDoPobraniaPage {
 
     }
 
+    /**
+   * Verifies if the user is currently on the "Downloads" page.
+   * @returns true if the URL matches the expected downloads page, false otherwise.
+   */
     async verifyUserIsOnPlikiDoPobraniaPage(): Promise<boolean> {
       
         return await isUrlMatches(this.page, 'http://www.selenium-shop.pl/moje-konto/downloads/');
         }
 
 
+    /**
+   * Checks if the informational message indicating that no downloads are available is correctly displayed.
+   * @returns true if the correct message is visible, false otherwise.
+   */
     async isInfoMessageDisplayedCorrectly(): Promise<boolean> {
         try {
         const infoMessageCorrect = await isElementVisibleWithPartialText(this.infoMessageDiv, 'Pliki do pobrania nie są jeszcze dostępne.');
