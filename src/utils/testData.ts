9596d7d91e4ea587c366f480c137b80f50649989
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2025-06-24 19:33:42 +0200

    added new object in testData.ts file - generated dynamically using faker package to ensure a consistent object structure with dynamically generated values

--- src/utils/testData.ts
+++ src/utils/testData.ts
@@ -1,11 +1,31 @@
-export const testData = {
-  imie: 'Maciej',
-  nazwisko: 'Jaroszewski',
-  plec: 'Kobieta',
-  wiek: '15-19',
-  produkt: 'Koszulka meczowa',
-  sport: 'koszykowka',
-  marka: 'adidas',
-  komentarz: 'hala madrid!',
-  dataZakupu: '10-06-2025',
-};
+import { faker } from '@faker-js/faker';
+
+const generateAnkietaData = () => ({
+  imie: faker.person.firstName('female'),
+  nazwisko: faker.person.lastName(),
+  plec: faker.helpers.arrayElement(['Kobieta', 'Mężczyzna']),
+  wiek: faker.helpers.arrayElement([
+    'mniej niż 15',
+    '15-19',
+    '20-29',
+    '30-39',
+    '40-60',
+    'więcej niż 60',
+  ]),
+  produkt: faker.helpers.arrayElement([
+    'Koszulka meczowa',
+    'Piłka nożna',
+    'Buty sportowe',
+    'Torba sportowa',
+  ]),
+  sport: faker.helpers.arrayElement(['pilkaNozna', 'siatkowka', 'koszykowka', 'bieganie', 'inny']),
+  marka: faker.helpers.arrayElement(['adidas', 'nike', 'kappa', 'lotto', 'puma']),
+  komentarz: faker.lorem.sentence(),
+  dataZakupu: faker.date
+    .future({ years: 1 })
+    .toLocaleDateString('pl-PL')
+    .replace(/\./g, '-')
+    .padStart(10, '0'),
+});
+
+export const testData = generateAnkietaData();
